3b9ae14b4e3af7166ab35e1ef3eea131e53aceb5
rlovasz <rlovasz@precognox.com>
2024-01-05 14:50:03 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'datetime-blur-bug-reproduction',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
